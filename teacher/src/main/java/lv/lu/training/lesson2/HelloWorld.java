37f92114f871309c2e5373b54a6c20b371ee6da7
Marina <mihejevamarina@gmail.com>
2022-04-06 21:03:15 +0300

    my first commit Marina

--- teacher/src/main/java/lv/lu/training/lesson2/HelloWorld.java
+++ teacher/src/main/java/lv/lu/training/lesson2/HelloWorld.java
@@ -2,8 +2,7 @@ package lv.lu.training.lesson2;
 
 public class HelloWorld {
 
-public static void main(String[] args){
-    System.out.println("Hello, World!");
-}
-
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
