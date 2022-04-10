e62b0faacaa0aaf7e0b8b42d650d43d32049a984
Radioonvich <102698758+Radioonvich@users.noreply.github.com>
2022-04-10 14:44:03 +0300

    TriangleAreaCalculator

---
+++ students/radionova-viktorija/src/main/java/lv/lu/training/lesson2/TringleAreaCalculator.java
@@ -0,0 +1,10 @@
+package lv.lu.training.lesson2;
+
+public class TringleAreaCalculator {
+    public static void main(String[] args) {
+        int base = 5;
+        int height = 10;
+        int result=(base*height)/2;
+        System.out.println(result);
+    }
+}
