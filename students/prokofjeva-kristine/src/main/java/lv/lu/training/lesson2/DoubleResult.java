77962ecd2c3cc668130c90dc61f8c5c5437f9bb3
Kristine <kristineprokofjeva@gmail.com>
2022-04-10 16:47:19 +0300

    Lesson 2, HomeWork1

---
+++ students/prokofjeva-kristine/src/main/java/lv/lu/training/lesson2/DoubleResult.java
@@ -0,0 +1,12 @@
+package lv.lu.training.lesson2;
+
+public class DoubleResult {
+    public static void main(String[] args) {
+        double x = 10;
+        int y = 4;
+        double result = x/(double)y;
+        System.out.println(result);
+
+    }
+
+}
