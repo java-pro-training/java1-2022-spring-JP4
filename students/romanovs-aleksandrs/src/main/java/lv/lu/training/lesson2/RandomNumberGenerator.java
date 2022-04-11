a0098fd90fc1abb5623c4a271bb210699140553e
Aleksandrs Romanovs <XapieC@gmail.com>
2022-04-11 22:23:53 +0300

    RandomNumberGenerator

--- students/romanovs-aleksandrs/src/main/java/lv/lu/training/lesson2/RandomNumberGenerator.java
+++ students/romanovs-aleksandrs/src/main/java/lv/lu/training/lesson2/RandomNumberGenerator.java
@@ -6,7 +6,7 @@ public class RandomNumberGenerator {
     public static void main(String[] args) {
         Random randomGenerator = new Random();
         int randomNumber = randomGenerator.nextInt(101);
-        System.out.println(randomNumber);
+        System.out.println(("Your Number:") + randomNumber);
 
     }
 }
