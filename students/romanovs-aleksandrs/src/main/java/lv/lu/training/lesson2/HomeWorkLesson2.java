a9dac92d091657266d02086d92a1fda42b3929a7
Aleksandrs Romanovs <XapieC@gmail.com>
2022-04-11 11:30:35 +0300

    HW Lesson 2.1 Aleksandrs Romanovs

---
+++ students/romanovs-aleksandrs/src/main/java/lv/lu/training/lesson2/HomeWorkLesson2.java
@@ -0,0 +1,22 @@
+package lv.lu.training.lesson2;
+
+import com.sun.source.util.SourcePositions;
+
+public class HomeWorkLesson2 {
+    public static void main(String[] args) {
+
+
+        int side1 = 10;
+        int side2 = 15;
+        int side3 = 14;
+        int side4 = 25;
+        int periment = 10;
+        int result = side1 + side3;
+        result += side4;
+        System.out.println("periment" + result);
+
+
+    }
+
+}
+
