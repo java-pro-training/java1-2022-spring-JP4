dbe576506fb8b5cdbeca6e82e320638e8913645a
Aleksandrs Romanovs <XapieC@gmail.com>
2022-04-11 11:35:15 +0300

    HW Lesson 2.0 Aleksandrs Romanovs

--- students/romanovs-aleksandrs/src/main/java/lv/lu/training/lesson2/HomeWorkLesson2.java
+++ students/romanovs-aleksandrs/src/main/java/lv/lu/training/lesson2/HomeWorkLesson2.java
@@ -6,14 +6,10 @@ public class HomeWorkLesson2 {
     public static void main(String[] args) {
 
 
-        int side1 = 10;
-        int side2 = 15;
-        int side3 = 14;
-        int side4 = 25;
-        int periment = 10;
-        int result = side1 + side3;
-        result += side4;
-        System.out.println("periment" + result);
+        String word1 = "Hello";
+        String word2 = "World!";
+        String result = word1 + " " + word2;
+        System.out.println(result);
 
 
     }
