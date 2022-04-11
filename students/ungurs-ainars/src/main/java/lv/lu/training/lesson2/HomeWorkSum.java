d81359ac3b462967aaaf08d71c20a443132735b5
AinarsUngurs <ainars.ungurs@gmail.com>
2022-04-11 20:49:14 +0300

    HW summa commit Ainars Ungurs

---
+++ students/ungurs-ainars/src/main/java/lv/lu/training/lesson2/HomeWorkSum.java
@@ -0,0 +1,10 @@
+package lv.lu.training.lesson2;
+
+public class HomeWorkSum {
+    public static void main(String[] args) {
+        int numberOne = 14;
+        int numberTwo = 57;
+        int resultSum = numberOne + numberTwo;
+        System.out.println(resultSum);
+    }
+}
