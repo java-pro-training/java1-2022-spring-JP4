bd6da3962cc164257dd66165d551a9b3f79be94d
normunds.rubeznieks <normunds.rubeznieks@gmail.com>
2022-04-12 23:52:58 +0300

    HM 2, Normunds Rubeznieks

--- students/rubeznieks-normunds/src/main/java/lv/lu/training/lesson2/HelloWorld.java
+++ students/rubeznieks-normunds/src/main/java/lv/lu/training/lesson2/HelloWorld.java
@@ -1,6 +1,6 @@
 package lv.lu.training.lesson2;
 
-public class HelloWorld {
+public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
