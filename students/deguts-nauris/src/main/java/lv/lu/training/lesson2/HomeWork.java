eb7318520df58538915459284f168ff766effc54
NaurisDeguts <deglis2@inbox.lv>
2022-04-13 16:47:20 +0300

    HW Lesson 2 Nauris

--- students/deguts-nauris/src/main/java/lv/lu/training/lesson2/HomeWork.java
+++ students/deguts-nauris/src/main/java/lv/lu/training/lesson2/HomeWork.java
@@ -1,8 +1,14 @@
 package lv.lu.training.lesson2;
 
+import com.sun.jdi.PathSearchingVirtualMachine;
+
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!!!");
+        String part1 = "Hello";
+        String part2 = "World";
+        String result = part1 + " " + part2;
+        System.out.println(result);
+
     }
 
     }
