673ed186823c435d314f51a01b8d3b8e4651e38f
ellevalery <ellevalery@gmail.com>
2022-04-19 11:50:38 +0300

    Triangle Area Calculator

---
+++ students/zalygina-valeria/src/main/java/lv/lu/training/lesson2/TriangleAreaCalculator.java
@@ -0,0 +1,11 @@
+package lv.lu.training.lesson2;
+
+public class TriangleAreaCalculator {
+    public static void main(String[] args) {
+        int base = 10;
+        int height = 20;
+        int result = ( base * height )/2;
+        System.out.println("Area of triangle is " +result);
+
+    }
+}
