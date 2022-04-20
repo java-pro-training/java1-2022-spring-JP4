f0e5ce90dc797c4b41613015253b149e50324c13
AinarsUngurs <ainars.ungurs@gmail.com>
2022-04-20 22:07:03 +0300

    CW 4 commit Ainars Ungurs

---
+++ students/ungurs-ainars/src/main/java/lv/lu/training/lesson4/QuickMathsTestRunner.java
@@ -0,0 +1,9 @@
+package lv.lu.training.lesson4;
+
+public class QuickMathsTestRunner {
+    public static void main(String[] args) {
+        QuickMathsTest testOne = new QuickMathsTest();
+        testOne.test1();
+        testOne.test2();
+    }
+}
