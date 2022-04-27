47e07316299e4604c75a41d533201a89a4c41d8d
nastyaaa005 <nastyaaa005@gmail.com>
2022-04-27 18:49:11 +0300

    HomeWork4 pt1

--- students/goncarova-anastasija/src/main/java/lv/lu/training/HMW4/RollDice.java
+++ students/goncarova-anastasija/src/main/java/lv/lu/training/HMW4/RollDice.java
@@ -7,13 +7,13 @@ public class RollDice {
 
         Random random = new Random();
 
-        int dice1 = random.nextInt (6) + 1;
-        int dice2 = random.nextInt (6) + 1;
+        int dice1 = random.nextInt(6) + 1;
+        int dice2 = random.nextInt(6) + 1;
         int result = dice1 + dice2;
 
         System.out.println("First number: " + dice1);
         System.out.println("Second number: " + dice2);
-        System.out.println("Result: "+ result);
+        System.out.println("Result: " + result);
 
         if (result == 7 || result == 11) {
             System.out.println("Winner!");
