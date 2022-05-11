e2a00330e4fcfd5cd9b048ff2ee5966f5d22f2eb
AleksandrsAndrejevs <aleksandrsandrejevs93@gmail.com>
2022-05-11 22:00:22 +0300

    HW 5 Aleksandrs Andrejevs

--- students/andrejevs-aleksandrs/src/main/java/lv/lu/training/lesson5/HW2/AccountUser.java
+++ students/andrejevs-aleksandrs/src/main/java/lv/lu/training/lesson5/HW2/AccountUser.java
@@ -1,11 +1,10 @@
 package lv.lu.training.lesson5.HW2;
 
-import java.util.Scanner;
+
 
 public class AccountUser {
     public static void main(String[] args) {
         BankAccount myAccount = new BankAccount("LV56PARX0030246807863", "4893", 150d, 300d, 0d);
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Mans konts " + myAccount);
         myAccount.withDraw(350d,"4894");
         System.out.println(myAccount);
