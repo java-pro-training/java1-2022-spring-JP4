9a09c1b226edd7ddac860be6e0f18a15015309b7
ellevalery <ellevalery@gmail.com>
2022-05-17 13:08:56 +0300

    CW5 Valeria Zalygina

---
+++ students/zalygina-valeria/src/main/java/lv/lu/training/lesson6/PlayWithContinue.java
@@ -0,0 +1,12 @@
+package lv.lu.training.lesson6;
+
+public class PlayWithContinue {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ students/zalygina-valeria/src/main/java/lv/lu/training/lesson6/PlayWithDoWhile.java
@@ -0,0 +1,35 @@
+package lv.lu.training.lesson6;
+
+import java.util.Scanner;
+
+public class PlayWithDoWhile {
+
+    public static void main(String[] args) {
+       // simpleDoWhile();
+        otherDoWhileExample();
+    }
+    private static void otherDoWhileExample() {
+        int sum = 0;
+        int number = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        do {
+            sum += number;
+            System.out.println("Please, enter some number");
+            number = scanner.nextInt();
+        }while (number>=0);
+        System.out.println("Sum= "+ sum);
+        scanner.close();
+    }
+
+
+    private static void simpleDoWhile() {
+        int i = 1;
+        int n = 5;
+        
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= n);
+    }
+}
