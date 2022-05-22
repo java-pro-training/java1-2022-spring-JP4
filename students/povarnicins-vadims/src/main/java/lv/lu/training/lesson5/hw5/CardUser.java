03dea50786d48f8f3e24baa608cbc273b194676a
VadimsPovarnicins <vadims.povarnicins@gmail.com>
2022-05-23 02:24:47 +0300

    Homework lesson 5

---
+++ students/povarnicins-vadims/src/main/java/lv/lu/training/lesson5/hw5/CardUser.java
@@ -0,0 +1,17 @@
+package lv.lu.training.lesson5.hw5;
+
+import java.time.LocalDate;
+
+public class CardUser {
+
+    public static void main(String[] args) {
+        BankCard myCard = new BankCard("Banka Citadele", "Visa", "Vadims Povarnicins", "33363533", LocalDate.of(2024, 12, 10), "333", 100);
+        System.out.println("Mana karte: " + myCard);
+        myCard.addFunds(100d);
+        System.out.println("Mana karte: " + myCard);
+        myCard.withdraw(20d);
+        System.out.println("Mana karte: " + myCard);
+    }
+
+
+}
