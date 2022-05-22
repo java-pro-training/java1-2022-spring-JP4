33c761dc161b808de4e404d3c2b530c8ef1d7034
AleksandrsAndrejevs <aleksandrsandrejevs93@gmail.com>
2022-05-22 18:45:23 +0300

    HW 7 Aleksandrs Andrejevs

--- students/andrejevs-aleksandrs/src/main/java/lv/lu/training/lesson7/PalindromeTest.java
+++ students/andrejevs-aleksandrs/src/main/java/lv/lu/training/lesson7/PalindromeTest.java
@@ -7,7 +7,7 @@ public class PalindromeTest {
     public static void main(String[] args) {
         System.out.println("Please enter a word to check if it is a palindrome");
         Scanner scanner = new Scanner(System.in);
-        if (Palindrome.isPalindrome(scanner.nextLine()) == true) {
+        if (Palindrome.isPalindrome(scanner.nextLine())) {
             System.out.println("Entered word is a palindrome");
         } else {
             System.out.println("Entered word in not a palindrome");
