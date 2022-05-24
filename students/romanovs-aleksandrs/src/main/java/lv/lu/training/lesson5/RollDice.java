4634fbdb3b4181038bcb5ccee5ede1cc86dc2388
Aleksandrs Romanovs <XapieC@gmail.com>
2022-05-24 19:39:19 +0300

    hw6

---
+++ students/romanovs-aleksandrs/src/main/java/lv/lu/training/lesson5/RollDice.java
@@ -0,0 +1,34 @@
+package lv.lu.training.lesson5;
+
+import java.util.Random;
+
+public class RollDice {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int dice1 = random.nextInt(6) + 1;
+        int dice2 = random.nextInt(6) + 1;
+
+        int diceSum = dice1 + dice2;
+        int sumOfWins = 0;
+        int sumOfLoose = 0;
+
+        while (diceSum != 12) {
+            if (diceSum == 7 || diceSum == 11) {
+                sumOfWins++;
+
+                System.out.println("you win");
+            }
+            if (diceSum == 2 || diceSum == 3 || diceSum == 6) {
+                sumOfLoose++;
+                System.out.println("You Loose");
+            }
+            dice1 = random.nextInt(6) + 1;
+            dice2 = random.nextInt(6) + 1;
+            diceSum = dice1 + dice2;
+        }
+        System.out.println("Wins: " + sumOfWins + " and losses: " + sumOfLoose);
+    }
+}
+
