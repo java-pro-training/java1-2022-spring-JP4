be4749bec702ea6e5aea1572025f837b960ae9be
nastyaaa005 <nastyaaa005@gmail.com>
2022-05-30 12:48:43 +0300

    HW7 pt.2

---
+++ students/goncarova-anastasija/src/main/java/lv/lu/training/lesson4/TestRunner.java
@@ -0,0 +1,9 @@
+package lv.lu.training.lesson4;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        QuickMathTest testone = new QuickMathTest();
+        testone.test1();
+        testone.test2();
+    }
+}
