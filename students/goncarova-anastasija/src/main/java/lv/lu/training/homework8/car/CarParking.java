20556ac5a77701da68cd7850e2f1bf38f65c8152
nastyaaa005 <nastyaaa005@gmail.com>
2022-05-31 15:41:07 +0300

    HW8 pt.1

---
+++ students/goncarova-anastasija/src/main/java/lv/lu/training/homework8/car/CarParking.java
@@ -0,0 +1,24 @@
+package lv.lu.training.homework8.car;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarParking {
+    public static void main(String[] args) {
+
+        Buss myNewBuss = new Buss("Black", 2020, "Mercedes", 50);
+        Buss myOldBuss = new Buss("Yellow", 1995, "Man", 35);
+        Truck myTruck = new Truck("White", 2015, "Volvo", 2500);
+        Car myCar = new Car("Blue", 2022, "BMW");
+
+        List<Car> allMyVenicles = Arrays.asList(myNewBuss, myOldBuss, myTruck, myCar);
+        for (Car car : allMyVenicles) {
+            System.out.println(car);
+
+
+            // System.out.println("My new buss: " + myNewBuss);
+            // System.out.println("My old buss: " + myOldBuss);
+            //  System.out.println("Are busses equals: " + myNewBuss.equals(myOldBuss));
+        }
+    }
+}
---
+++ students/goncarova-anastasija/src/main/java/lv/lu/training/homework8/car/Passenger.java
@@ -0,0 +1,5 @@
+package lv.lu.training.homework8.car;
+
+public interface Passenger {
+    int passengerSeatCount();
+}
