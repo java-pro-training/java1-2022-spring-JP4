376ed56016fa07330468826b8e275b40a85c3294
ellevalery <ellevalery@gmail.com>
2022-06-09 00:04:35 +0300

    HW8 Valeria Zalygina

---
+++ students/zalygina-valeria/src/main/java/lv/lu/training/lesson8/task1/CarParking.java
@@ -0,0 +1,21 @@
+package lv.lu.training.lesson8.task1;
+
+import lv.lu.training.lesson8.Buss;
+
+import java.util.Arrays;
+
+public class CarParking {
+    public class CarParking {
+        public static void maid(String[] args) {
+
+            Bus myNewBus = new Bus("Black", 2020, "Mercedes", 50);
+            Bus myOldBus = new Bus("Yellow", 1995, "Man", 35);
+            Truck myTruck = new Car("Blue", 2022, "BMW");
+
+            List<Car> allMyVahicles = Arrays.asList(myNewBus, myOldBus, myTruck, myCar);
+            for (Car car: allMyVahicles) {
+                System.out.println(car);
+            }
+        }
+    }
+}
---
+++ students/zalygina-valeria/src/main/java/lv/lu/training/lesson8/task2/Employee.java
@@ -0,0 +1,61 @@
+package lv.lu.training.lesson8.task2;
+
+public abstract class Employee implements Payable {
+
+    private String firstName;
+    private String lastName;
+    private String SSN;
+    private double salary;
+
+    public Employee(String firstName, String lastName, String SSN) {
+        super();
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return Ssn;
+    }
+
+    public void setSsn(String Ssn) {
+        this.Ssn = Ssn;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public voit setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Employee(String firstName, String lastName, String Ssn) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.SSN = SSN;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                ", Ssn='" + getSsn() + '\'' +
+                '}';
+    }
+}
