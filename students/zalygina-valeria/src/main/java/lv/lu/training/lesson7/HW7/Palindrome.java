a921468691b2190041bbf210218fde1ee42dab2b
ellevalery <ellevalery@gmail.com>
2022-06-08 10:17:00 +0300

    CW7 Valeria Zalygina

---
+++ students/zalygina-valeria/src/main/java/lv/lu/training/lesson7/HW7/Palindrome.java
@@ -0,0 +1,17 @@
+package lv.lu.training.lesson7.HW7;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Palindrome {
+  public static boolean isPalindrome(String text) {
+      String input = text.toLowerCase();
+      char[] original = input.toCharArray();
+      char[] reverse = new char[text.length()];
+      for (int i = text.length() - 1, j =0; i >= 0; j++){
+          reverse[j] = original[i];
+      }
+      return Arrays.equals(reverse, original);
+  }
+}
+
