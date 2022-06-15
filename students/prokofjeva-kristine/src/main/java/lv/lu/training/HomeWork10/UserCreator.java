cb80a356ebdfdc7be04a13d2477763ae683d3581
Kristine <kristineprokofjeva@gmail.com>
2022-06-15 11:52:07 +0300

    HomeWork10

---
+++ students/prokofjeva-kristine/src/main/java/lv/lu/training/HomeWork10/UserCreator.java
@@ -0,0 +1,9 @@
+package lv.lu.training.HomeWork10;
+
+public class UserCreator {
+    public static void main(String[] args) {
+        UserValidator validator = new UserValidator();
+     //   validator.validate(new User("Kristine ", "Prokofjeva", 25));
+        validator.validate(new User("aaaaa","ddfddsf", 244));
+    }
+}
