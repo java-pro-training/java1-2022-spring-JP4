8ae9ed7647269f8acd44adee02ddb53a31881958
Radioonvich <102698758+Radioonvich@users.noreply.github.com>
2022-06-20 23:15:38 +0300

    HomeWork6

---
+++ students/radionova-viktorija/src/main/java/lv/lu/training/HW6/FindingOddNumber.java
@@ -0,0 +1,43 @@
+package lv.lu.training.HW6;
+
+public class FindingOddNumber {
+
+    public int from;
+    public int till;
+
+    public FindingOddNumber(int from, int till) {
+        this.from = from;
+        this.till = till;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTill() {
+        return till;
+    }
+
+    public static boolean isOdd(int number) {
+        return (number % 2 != 0);
+    }
+
+    public static void main(String[] args) {
+
+        FindingOddNumber interval = new FindingOddNumber(12, 42);
+
+        if (interval.getFrom() > interval.getTill()) {
+            System.out.println("Incorrect: ");
+            System.out.println("Number in the range :");
+            System.out.println("Please, try again: ");
+        } else {
+            for (int i = interval.getFrom(); i <= interval.getTill(); i++) {
+                if (isOdd(i)) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+
+}
+
--- students/radionova-viktorija/src/main/java/lv/lu/training/HW6/RollDice.java
+++ students/radionova-viktorija/src/main/java/lv/lu/training/HW6/RollDice.java
@@ -5,21 +5,21 @@ import java.util.Random;
 public class RollDice {
     public static void main(String[] args) {
 
-    int sumOfWins = 0;
-    rollDice();
-    while (rollDice() != 12) {
-        if ((rollDice() == 7) || (rollDice() == 11)) {
-            sumOfWins++;
-            System.out.println("You win: " + sumOfWins );
-            rollDice();
-        } else if ((rollDice() == 2) || (rollDice() == 3) || (rollDice() == 6)) {
-            System.out.println("You loose: " + sumOfWins );
-            rollDice();
-        } else {
-            rollDice();
+        int sumOfWins = 0;
+        rollDice();
+        while (rollDice() != 12) {
+            if ((rollDice() == 7) || (rollDice() == 11)) {
+                sumOfWins++;
+                System.out.println("You win: " + sumOfWins);
+                rollDice();
+            } else if ((rollDice() == 2) || (rollDice() == 3) || (rollDice() == 6)) {
+                System.out.println("You loose: " + sumOfWins);
+                rollDice();
+            } else {
+                rollDice();
+            }
         }
     }
-}
 
     public static int rollDice() {
         Random random = new Random();
