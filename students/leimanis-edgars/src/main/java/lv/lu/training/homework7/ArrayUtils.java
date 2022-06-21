10874e51b69df8e7de826eb72473103724e67ae7
EdgarsLeimanis <edgars9218@inbox.lv>
2022-06-21 23:09:00 +0300

    homework7

--- students/leimanis-edgars/src/main/java/lv/lu/training/homework7/ArrayUtils.java
+++ students/leimanis-edgars/src/main/java/lv/lu/training/homework7/ArrayUtils.java
@@ -9,16 +9,16 @@ public class ArrayUtils {
     private int intA, intB;
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(reverse(new int[]{1,2,3})));
+        System.out.println(Arrays.toString(reverse(new int[]{1, 2, 3})));
     }
 
     private static int[] reverse(int[] initial) {
         int[] result = new int[initial.length];
-        for(int i = initial.length -1, j=0; i>=0; i--, j++){
+        for (int i = initial.length - 1, j = 0; i >= 0; i--, j++) {
             result[j] = initial[i];
-    }
-return result;
+        }
+        return result;
 
-}
+    }
 
 }
