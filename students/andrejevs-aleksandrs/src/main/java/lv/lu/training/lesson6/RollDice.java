d75304f85c8fd8079c37a41e8af63aec69df63eb
AleksandrsAndrejevs <aleksandrsandrejevs93@gmail.com>
2022-06-22 00:16:05 +0300

    Aleksandrs Andrejevs HW11

--- students/andrejevs-aleksandrs/src/main/java/lv/lu/training/lesson6/RollDice.java
+++ students/andrejevs-aleksandrs/src/main/java/lv/lu/training/lesson6/RollDice.java
@@ -1,23 +1,24 @@
 package lv.lu.training.lesson6;
 
 import java.util.Random;
+
 //LABOT
 //rollDice metode izmantota vietām nekorekti
 public class RollDice {
 
     public static void main(String[] args) {
         int sumOfWins = 0;
-        rollDice(); //šeit jūs varat dabūt vērtību no metodes un izmantot tālāk
-        while (rollDice() != 12) {
-            if ((rollDice() == 7) || (rollDice() == 11)) { //jūs pārsetojat result vērtību katru reizi, kad izsaucat rolldice
+        int diceNumber = rollDice(); //šeit jūs varat dabūt vērtību no metodes un izmantot tālāk
+        while (diceNumber != 12) {
+            if ((diceNumber == 7) || (diceNumber == 11)) { //jūs pārsetojat result vērtību katru reizi, kad izsaucat rolldice
                 sumOfWins++;
                 System.out.println("Congratulations, you are the winner! You have won a total of " + sumOfWins + " times");
-                rollDice();
-            } else if ((rollDice() == 2) || (rollDice() == 3) || (rollDice() == 6)) {
+                diceNumber = rollDice();
+            } else if ((diceNumber == 2) || (diceNumber == 3) || (diceNumber == 6)) {
                 System.out.println("Unfortunately, you have lost. You have won a total of " + sumOfWins + " times");
-                rollDice();
+                diceNumber = rollDice();
             } else {
-                rollDice();
+                diceNumber = rollDice();
             }
         }
     }
