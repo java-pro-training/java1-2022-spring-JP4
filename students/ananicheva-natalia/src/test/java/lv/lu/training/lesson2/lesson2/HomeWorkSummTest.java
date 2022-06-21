a0f02853583af55ad8d99bada1d49ecb5bf2f435
NatalyaAnanicheva <natalya_ananiche@mail.ru>
2022-06-21 19:11:38 +0300

    lesson10

---
+++ students/ananicheva-natalia/src/test/java/lv/lu/training/lesson2/lesson2/HomeWorkSummTest.java
@@ -0,0 +1,23 @@
+package lv.lu.training.lesson2.lesson2;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeWorkSummTest {
+    private HomeWorkSumm victim;
+    @Before
+    public void SetUp(){
+        victim=new HomeWorkSumm();
+    }
+@Test
+    public void shouldReturnTotalOfTwoNumbers(){
+    Class<? extends HomeWorkSumm> result=victim.getClass();
+    Assert.assertEquals(80,result);
+
+}
+
+}
